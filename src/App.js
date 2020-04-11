9cce1d1dfd659630994192c81a16fdf8e03654ad
Gabriel Chüler Battistella <gabriel.chulerbattistella@gmail.com>
2020-04-11 19:28:48 -0300

    First code in React to learn the basics

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Tweet from './Tweet';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+
+  const [users, setUsers] = useState([
+    {name: 'Gabriel', message: 'Hello there'},
+    {name: 'João', message: 'Just another quote'},
+    {name: 'Test', message: 'Lorem ipsum'}
+  ]);
+
+  return(
+    <div className="app">
+      {users.map(user => (
+        <Tweet name={user.name} message={user.message}/>
+      ))}
     </div>
-  );
+  )
 }
 
 export default App;
